83775a122fc6563846fa353d19fae80a87aabea3
twooliston <70709801+twooliston@users.noreply.github.com>
2022-03-22 22:39:30 +0000

    clean

--- src/components/Header.js
+++ src/components/Header.js
@@ -52,7 +52,6 @@ const Header = () => {
       <aside className="sidebar">
         <div className="sidebar-header">
           <div className="width-100"></div>
-          {/* <h3 className="width-100">Barocas Academy</h3> */}
           <button className="close-burger-menu" onClick={closeMenu}>
             close
           </button>

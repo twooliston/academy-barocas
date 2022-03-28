990914d4760f7e8c5f46f985bd04a896680a4b33
twooliston <70709801+twooliston@users.noreply.github.com>
2022-03-28 21:16:55 +0100

    build temp email

--- src/pages/footer/Contact.js
+++ src/pages/footer/Contact.js
@@ -5,13 +5,32 @@ const Contact = () => {
   return (
     <div className="page long-text">
       <h1>Contact Barocas</h1>
-      <p>
+      {/* <p>
         If you have any questions, please see the <Link to="/faqs">FAQs</Link>.
         If your questions are still not answered, please fill out the
         information below:
-      </p>
-      <h1>Currently Unavailable</h1>
-      <h1>Please check back later</h1>
+      </p> */}
+      <form
+        action="mailto:twooliston@gmail.com?subject=Contact Academy Barocas&body=Hi,"
+        method="post"
+        enctype="text/plain"
+        className="padding-100"
+      >
+        {/* <label>Name:</label>
+        <br />
+        <input type="text" name="name" />
+        <br />
+        <label>E-mail:</label>
+        <br />
+        <input type="text" name="mail" />
+        <br />
+        <label>Comment:</label>
+        <br />
+        <textarea name="comment" rows="6" cols="40" />
+        <input type="text" name="comment" size="50" />
+        <br /> */}
+        <input type="submit" value="Send Email" className="button" />
+      </form>
     </div>
   );
 };
